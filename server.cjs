4bbbc697ec28b424f10f8c587308407152418ed1
Kay Hsu <kayhsu@Kays-MacBook-Pro.local>
2025-03-03 10:19:38 -0800

    v2

--- server.cjs
+++ server.cjs
@@ -3,10 +3,11 @@ const{fetchAllBagels, fetchBagelDetails} = require('./db/bagels.cjs');
 const express = require('express');
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('dist')); 
 
 const client = require('./db/client.cjs');
 client.connect();
+
 
 // get all bagel info
 app.get('/api/bagels', async(req, res, next) => {
@@ -16,7 +17,8 @@ app.get('/api/bagels', async(req, res, next) => {
   } catch(err) {
     next(err);
   }
-});
+}); 
+
 
 // get certain bagel details
 app.get('/api/bagels/:bagel_id', async(req, res, next) => {
